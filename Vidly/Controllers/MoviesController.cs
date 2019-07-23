ec24c355b5dbb97b49424e40ab96ab60d2efb853
Angel Gabriel <angelgabrielda@hotmail.com>
2019-07-23 00:32:26 -0500

    se agrego la actualizacion de boostrap y un enrutamiento de año y mes

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -15,5 +15,12 @@ namespace Vidly.Controllers
             var movie = new Movie() { Name = "Shrek!" };
             return View(movie);
         }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+
+
+        }
     }
 }
