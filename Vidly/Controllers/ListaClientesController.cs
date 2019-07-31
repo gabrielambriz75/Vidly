494d5d836431fc1da86d754740e6729c5fe066c4
Angel Gabriel <angelgabrielda@hotmail.com>
2019-07-31 12:05:29 -0500

    Se agrego la lista de Clientes a la vista

---
+++ Vidly/Controllers/ListaClientesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.Clientes;
+
+namespace Vidly.Controllers
+{
+    public class ListaClientesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Index()
+        {
+            var listaClientes = new ListaClientes();
+            var model = listaClientes.ObtenerClientes();
+            return View(model);
+
+        }
+
+        
+      
+    }
+}
