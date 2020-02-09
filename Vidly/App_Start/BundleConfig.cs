a7c9cddda90013ea6a8fa6feb4465237b8498b2a
Angel Gabriel <angelgabrielda@hotmail.com>
2020-02-09 02:33:22 -0600

    Plugin dataTable

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -8,8 +8,14 @@ namespace Vidly
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootbox.js",
+                      "~/Scripts/respond.js",
+                      "~/Scripts/Datatables/media/js/jquery.dataTables.js",
+                      "~/Scripts/DataTables/media/js/dataTables.bootstrap4.js")
+                     );
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -19,14 +25,11 @@ namespace Vidly
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/bootbox.js",
-                      "~/Scripts/respond.js"
-                      ));
+            
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/lumen.css",
+                       "~/Content/DataTables/media/css/dataTables.bootstrap4.css",
                       "~/Content/site.css"));
         }
     }
